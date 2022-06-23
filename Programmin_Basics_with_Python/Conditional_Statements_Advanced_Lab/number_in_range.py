dbe2824efe276cf741ccd9b149fbf9df998f7b8c
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-06-23 18:50:05 +0300

    added a code

---
+++ Programmin_Basics_with_Python/Conditional_Statements_Advanced_Lab/number_in_range.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+if num >= -100 and num <= 100 and num != 0:
+    print('Yes')
+else:
+    print('No')
+
+
+#Тук сега ще покажем с вложена проверка
+
+#if num >= - 100:
+    #if num <= 100:
+        #if num != 0:
+            #print('Yes')
+        #else:
+            #print('No')
