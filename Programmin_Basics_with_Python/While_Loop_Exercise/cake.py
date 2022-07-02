dd3b5eb49b259dbfd55ad793c44b5ed7ede3c6ab
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-07-02 19:32:25 +0300

    Added code

---
+++ Programmin_Basics_with_Python/While_Loop_Exercise/cake.py
@@ -0,0 +1,20 @@
+width = int(input())
+length = int(input())
+
+total_pieces = width * length
+
+input_line = input()
+while input_line != "STOP":
+    current_pieces = int(input_line)
+
+    total_pieces = total_pieces - current_pieces
+
+    if total_pieces <= 0:
+        break
+    input_line = input()
+
+if total_pieces > 0:
+    print(f"{total_pieces} pieces are left.")
+else:
+
+    print(f"No more cake left! You need {abs(total_pieces)} pieces more.")
