80f14d14f0ef5530750a36c1f9d8d09c633d8e0f
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-07-05 21:35:47 +0300

    Added code

---
+++ Programmin_Basics_with_Python/Nested_Loops_Lab/travelling.py
@@ -0,0 +1,13 @@
+destination = input()
+
+while destination != 'End':
+    price_trip = float(input())
+
+    sum = 0
+    while sum < price_trip:
+        saved_money = float(input())
+        sum = sum + saved_money
+
+    print(f'Going to {destination}!')
+
+    destination = input()
