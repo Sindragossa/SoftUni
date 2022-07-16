cbb68e000671e82fb3457f940f951b420273cf12
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-07-16 22:22:28 +0300

    Added code

---
+++ Programmin_Basics_with_Python/PB_More_Exercises/For_Loop_More_Exercises/equal_pairs.py
@@ -0,0 +1,25 @@
+num_pairs = int(input())
+
+max_diff = 0
+current_sum = 0
+sum_first_pair = 0
+diff = 0
+
+for numbers in range(1, 2 * num_pairs + 1):
+    number = int(input())
+    current_sum += number
+
+    if numbers % 2 == 0 and numbers != 2:
+        diff = abs(current_sum - sum_first_pair)
+        if diff > max_diff:
+            max_diff = diff
+        sum_first_pair = current_sum
+        current_sum = 0
+    elif numbers == 2:
+        sum_first_pair = current_sum
+        current_sum = 0
+
+if max_diff == 0:
+    print(f'Yes, value={sum_first_pair}')
+else:
+    print(f'No, maxdiff={max_diff}')
