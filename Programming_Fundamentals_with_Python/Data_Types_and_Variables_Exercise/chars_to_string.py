c7bcc80a28775d52f2e538a981330eb917387587
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-07-23 22:00:20 +0300

    Added code

---
+++ Programming_Fundamentals_with_Python/Data_Types_and_Variables_Exercise/chars_to_string.py
@@ -0,0 +1,6 @@
+first_characters = input()
+second_characters = input()
+third_characters = input()
+
+all_characters = first_characters + second_characters + third_characters
+print(all_characters)
