5fbde6ea8232a4ace9636c92b7c80d0e83ef571e
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-08-02 20:22:19 +0300

    Added code

---
+++ Programming_Fundamentals_with_Python/Functions_Lab/calculate_rectangle_area.py
@@ -0,0 +1,8 @@
+def area_of_a_rectangle(width, height):
+    return width * height
+
+
+width_input = int(input())
+height_input = int(input())
+
+print(area_of_a_rectangle(width_input, height_input))
