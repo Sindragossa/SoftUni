0361e5211f6ac076210a8e627f043f6a00d9dddb
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-08-17 19:55:09 +0300

    Added code

---
+++ Programming_Fundamentals_with_Python/Dictionaries_Exercise/softuni_parking.py
@@ -0,0 +1,24 @@
+numbers_of_cars = int(input())
+parking = {}
+
+for c in range(numbers_of_cars):
+    command = input().split()
+    username = command[1]
+
+    if command[0] == 'register':
+        license_plate = command[2]
+
+        if username not in parking:
+            parking[username] = license_plate
+            print(f'{username} registered {license_plate} successfully')
+        else:
+            print(f'ERROR: already registered with plate number {license_plate}')
+    elif command[0] == 'unregister':
+        if username not in parking:
+            print(f'ERROR: user {username} not found')
+        else:
+            print(f'{username} unregistered successfully')
+            del parking[username]
+
+for user, plate in parking.items():
+    print(f'{user} => {plate}')
