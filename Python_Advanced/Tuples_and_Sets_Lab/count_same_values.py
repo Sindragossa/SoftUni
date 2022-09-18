0aa02840f99fb1e913fb317dbac5677d0ffe74f5
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-09-18 21:35:18 +0300

    Added code

---
+++ Python_Advanced/Tuples_and_Sets_Lab/count_same_values.py
@@ -0,0 +1,10 @@
+numbers = tuple(map(float, input().split()))
+values_count = {}
+
+for value in numbers:
+    if value not in values_count:
+        values_count[value] = 0
+    values_count[value] += 1
+
+for (value, count) in values_count.items():
+    print(f'{value} - {count} times')
