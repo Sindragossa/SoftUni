3fba0ede4d2b29884872321ef68bdf48bfc8ef6b
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-10-07 19:15:23 +0300

    Added code

---
+++ Python_Advanced/Functions_Advanced_Exercise/recursion_palindrome.py
@@ -0,0 +1,10 @@
+def palindrome(word: str, index=0):
+    if index == len(word) // 2:
+        return f'{word} is a palindrome'
+
+    if not word[index] == word[(len(word) - index) - 1]:
+        return f'{word} is not a palindrome'
+    return palindrome(word, index + 1)
+
+
+print(palindrome('abcdba', 0))
