a6d8dfc3c32575f1e58aafc3275b2bcaa2994c7b
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2022-10-08 21:52:19 +0300

    Added code

---
+++ Python_Advanced/Multidimensional_Lists_Exercise_1/diagonals.py
@@ -0,0 +1,10 @@
+rows = int(input())
+matrix = [input().split(', ') for i in range(rows)]
+primary_diagonal, secondary_diagonal = [], []
+
+for idx in range(rows):
+    primary_diagonal.append(matrix[idx][idx])
+    secondary_diagonal.append(matrix[idx][rows - idx - 1])
+
+print(f'Primary diagonal: {", ".join(primary_diagonal)}. Sum: {sum(map(int,primary_diagonal))}')
+print(f'Secondary diagonal: {", ".join(secondary_diagonal)}. Sum: {sum(map(int,secondary_diagonal))}')
