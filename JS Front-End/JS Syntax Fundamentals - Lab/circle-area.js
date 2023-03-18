3305ab277816da52f165ddc2f76a2b1a434a2cf2
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2023-03-18 21:54:36 +0200

    added code

---
+++ JS Front-End/JS Syntax Fundamentals - Lab/circle-area.js
@@ -0,0 +1,10 @@
+function circleArea(radius) {
+
+    if (typeof radius === 'number') {
+        let area = radius * radius * Math.PI
+        console.log(area.toFixed(2))
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${typeof radius}.`)
+    }
+
+}
