6b4a410014b8f84693c2a03bc27dbf1bb8a75c64
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2023-05-03 15:20:55 +0300

    added code

---
+++ JS Front-End/JS Syntax Fundamentals - Exercises/13. arrayRotation.js
@@ -0,0 +1,8 @@
+function arrayRotation(array, rotations) {
+    for (let i = 1; i <= rotations; i++) {
+      element = array.shift();
+      array.push(element);
+    }
+  
+    console.log(...array);
+  }
