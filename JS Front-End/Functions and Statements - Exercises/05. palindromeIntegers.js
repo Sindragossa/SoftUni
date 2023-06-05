7ac80be873f2f46b85eba5acd8f647b79c58eda5
Nadezhda Yordanova <yordanovanadejda98@gmail.com>
2023-06-05 14:01:17 +0300

    added code

---
+++ JS Front-End/Functions and Statements - Exercises/05. palindromeIntegers.js
@@ -0,0 +1,13 @@
+function palindromeIntegers(arr) {
+    for (let el of arr) {
+       let reversed = String(el).split('').reverse().join('');
+
+        if (el == reversed) {
+            console.log('true')
+            
+        } else {
+            console.log('false')
+        }
+
+    }
+}
